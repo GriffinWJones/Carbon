6033da13cb5146127416fe5006abacb030ea5bc5
Evan Wong <evanbradleywong1@gmail.com>
2023-11-11 18:57:07 -0800

    h

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,15 @@
-import 'dart:js';
-
 import 'package:flutter/material.dart';
 import 'package:inrixecocoin2/test_widget.dart';
 import 'home.dart';
 import 'test_data.dart';
+import 'loading.dart';
 
 void main() => runApp(MaterialApp(
-      initialRoute: '/test',
+      initialRoute: '/loading',
       routes: {
         '/': (context) => Test(),
         '/home': (context) => Home(user: testUsers[0]),
+        '/loading': (context) => Loading(),
         '/test': (context) => Test(),
       },
     ));
