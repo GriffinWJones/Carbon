6a3587b9b4eee1381fbe92f434df400077d493dd
Matty Herzig <mattyherzig@yahoo.com>
2023-11-11 16:59:28 -0800

    MESSAGE

---
+++ lib/api_testing.dart
@@ -0,0 +1,25 @@
+// import 'package:flutter/material.dart';
+
+// temporary file, for testing API
+
+// var headers = {
+//   'Authorization': 'Bearer eTZlamExYmV6OXxqTmFFaFVBM1dQYWJDT1hXTlNsczgyR2dwNzFYa3hSZzlwdHdRcVEy'
+// };
+// var request = http.Request('GET', Uri.parse('https://api.iq.inrix.com/auth/v1/appToken?appId=y6eja1bez9&hashToken=eTZlamExYmV6OXxqTmFFaFVBM1dQYWJDT1hXTlNsczgyR2dwNzFYa3hSZzlwdHdRcVEy'));
+
+// request.headers.addAll(headers);
+
+// http.StreamedResponse response = await request.send();
+
+// if (response.statusCode == 200) {
+//   print(await response.stream.bytesToString());
+// }
+// else {
+//   print(response.reasonPhrase);
+// }
+
+
+
+
+// TEST! NEW FILE!
+
